cd3a75e2b13ca0ba80685a9818244011c67fa076
aysenur-ay <74199538+aysenur-ay@users.noreply.github.com>
2021-01-09 16:39:57 -0500

    Update UserStory_1.java
    I just erase a dot just trying to make comment

--- src/test/java/batch21_group29/UserStory_1.java
+++ src/test/java/batch21_group29/UserStory_1.java
@@ -2,6 +2,6 @@ package batch21_group29;
 
 public class UserStory_1 {
     public static void main(String[] args) {
-        System.out.println("Hello my friends...!");
+        System.out.println("Hello my friends..!");
     }
 }
